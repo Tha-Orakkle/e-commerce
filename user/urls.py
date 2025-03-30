1b6e0d0c2127400b096ae05499610d691092e980
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-03-30 05:46:06 +0100

    update: Add the path for verify email address

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from django.urls import path
 
-from .tokens import SecureTokenRefreshView
-from .views import RegisterView, LoginView, UserView
-from .social_auth_views import GoogleLogin
+from .routes.refresh_tokens import SecureTokenRefreshView
+from .routes.login_register import RegisterView, LoginView, UserView
+from .routes.verify_email import VerifyEmailView
+from .routes.google_oauth import google_login, google_callback
 
 urlpatterns = [
     # registration and tokens generation
@@ -11,10 +12,14 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', SecureTokenRefreshView.as_view(), name='token_refresh'),
 
-    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    # sign in with google
+    path('auth/google/login/', google_login),
+    path('auth/callback/google/', google_callback),
+
+    # email address verification
+    path('verify/', VerifyEmailView.as_view(), name='verify_email'),
 
     # User
     path('users/', UserView.as_view(), name='user'),
     path('users/<str:id>/', UserView.as_view(), name='user'),
-
 ]
