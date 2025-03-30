c703796d9811319bf6447f6de98c9c84770517c9
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-03-30 05:32:00 +0100

    feat: add background task to send email address verification mail

---
+++ user/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+
+from user.utils.email_verification import generate_email_verification_token
+from user.utils.send_email import send_verification_mail
+
+
+@shared_task
+def send_verification_mail_task(user_id, email):
+    token = generate_email_verification_token(user_id)
+    link = f'http://127.0.0.1/api/verify/?token={token}'
+    send_verification_mail(email, link)
