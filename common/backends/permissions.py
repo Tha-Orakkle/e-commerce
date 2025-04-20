7e97bb116825b3a311d6db4ed9aadc0d88113dc3
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-04-21 00:22:25 +0100

    update: add doc string comment to method

--- common/backends/permissions.py
+++ common/backends/permissions.py
@@ -1,8 +1,12 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
 
 class IsSuperUser(BasePermission):
     """
     Grants permission to only super users.
     """
     def has_permission(self, request, view):
+        """
+        Checks if user is super user.
+        """
         return bool(request.user and request.user.is_superuser)
