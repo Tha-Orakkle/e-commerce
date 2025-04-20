0e0880ef664ce64628302e5d790a9fa0456e460a
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-04-21 00:30:37 +0100

    feat: test put and delte user

--- user/tests/test_user.py
+++ user/tests/test_user.py
@@ -9,11 +9,11 @@ from rest_framework import status
 User = get_user_model()
 
 
-def test_get_users_by_admin(client, user, signed_in_admin):
+def test_get_users(client, user, signed_in_user):
     """
     Test get all users by admin.
     """
-    tokens = signed_in_admin
+    tokens = signed_in_user
     url = reverse('users')
     client.cookies['access_token'] == tokens['access_token']
     response = client.get(url)
@@ -21,21 +21,7 @@ def test_get_users_by_admin(client, user, signed_in_admin):
     assert response.data['status'] == "success"
     assert response.data['code'] == 200
     assert response.data['message'] == "Users retrieved successfully."
-    assert response.data['data']['count'] == 2
-
-
-def test_get_users_by_non_admin(client, signed_in_user):
-    """
-    Test get all users by non admin.
-    """
-    tokens = signed_in_user
-    url = reverse('users')
-    client.cookies['access_token'] == tokens['access_token']
-    response = client.get(url)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert response.data['status'] == "error"
-    assert response.data['code'] == 403
-    assert response.data['message'] == "You do not have permission to perform this action."
+    assert response.data['data']['count'] == 1
 
 
 def test_get_user_with_id(client, user, signed_in_user):
@@ -48,44 +34,13 @@ def test_get_user_with_id(client, user, signed_in_user):
     response = client.get(url)
     assert response.status_code == status.HTTP_200_OK
     assert response.data['status'] == "success"
+    assert response.data['code'] == 200
     assert response.data['message'] == "User retrieved successfully."
     assert response.data['data']
     assert response.data['data']['id'] == str(user.id)
-
-
-def test_get_user_by_admin(client, user, signed_in_admin):
-    """
-    test get another usr by an admin user.
-    """
-    url = reverse('user', kwargs={'id': user.id})
-    tokens = signed_in_admin
-    client.cookies['access_token'] = tokens['access_token']
-    response = client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.data['status'] == "success"
-    assert response.data['message'] == "User retrieved successfully."
-    assert response.data['data']
-    assert response.data['data']['id'] == str(user.id)
-
-
-def test_get_user_by_non_admin(client, signed_in_user):
-    """
-    Test get another user by a non admin user.
-    """
-    user = User.objects.create_user(
-        email="test@email.com",
-        password="Password123#")
-    url = reverse('user', kwargs={'id': user.id})
-    tokens = signed_in_user
-    client.cookies['access_token'] = tokens['access_token']
-    response = client.get(url)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert response.data['status'] == "error"
-    assert response.data['code'] == 403
-    assert response.data['message'] == "You do not have permission to perform this action."
 
     
-def test_get_user_with_invalid_id(client, user, signed_in_user):
+def test_get_user_with_invalid_id(client, signed_in_user):
     """
     Test get user with invalid id.
     """
@@ -95,6 +50,7 @@ def test_get_user_with_invalid_id(client, user, signed_in_user):
     response = client.get(url)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['status'] == "error"
+    assert response.data['code'] == 400
     assert response.data['message'] == "Invalid user id."
 
 
@@ -108,6 +64,7 @@ def test_put_user_email(mock_verification_email_task, client, user, signed_in_us
     client.cookies['access_token'] = tokens['access_token']
     response = client.put(url, data, format='json')
     mock_verification_email_task.assert_called_once()
+    print(response.data['message'])
     assert response.status_code == status.HTTP_200_OK
     assert response.data['code'] == 200
     assert response.data['status'] == "success"
@@ -141,25 +98,22 @@ def test_put_user_email_with_same_verified_email(mock_verification_email_task, c
     assert response.data['data']['is_verified'] == True
 
 
-def test_put_user_password(mock_verification_email_task, client, user, signed_in_user):
+def test_put_user_with_invalid_id(client, signed_in_user):
     """
-    Test update user password.
+    Test update user with invalid id.  
     """
-    url = reverse('user', kwargs={'id': user.id})
-    tokens = signed_in_user
+    url = reverse('user', kwargs={'id': "123-Invalid-id"})
     data = {
-        'password': 'Example123#',
-        'confirm_password': 'Example123#'
+        'passord': 'Password1234#',
+        'confirm_passord': 'Password1234#'
     }
-    client.cookies['access_token'] = tokens['access_token']
-    response = client.put(url, data, format='json')
-    mock_verification_email_task.assert_not_called()
-    assert response.status_code == status.HTTP_200_OK
-    assert response.data['code'] == 200
-    assert response.data['status'] == "success"
-    assert response.data['message'] == "User updated successfully."
-    assert response.data['data']
-    assert response.data['data']['id'] == str(user.id)
+    client.cookies['access_token'] = signed_in_user['access_token']
+    response = client.get(url)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data['status'] == "error"
+    assert response.data['code'] == 400
+    assert response.data['message'] == "Invalid user id."
+
 
 
 def test_put_user_email_invalid_email(mock_verification_email_task, client, user, signed_in_user):
@@ -177,7 +131,7 @@ def test_put_user_email_invalid_email(mock_verification_email_task, client, user
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['email']
     assert response.data['errors']['email'][0] == "Enter a valid email address."
 
@@ -201,12 +155,31 @@ def test_put_user_email_existing_email(mock_verification_email_task, client, use
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['email']
     assert response.data['errors']['email'][0] == "user with this email already exists."
 
 
-# test password strength
+def test_put_user_password(mock_verification_email_task, client, user, signed_in_user):
+    """
+    Test update user password.
+    """
+    url = reverse('user', kwargs={'id': user.id})
+    tokens = signed_in_user
+    data = {
+        'password': 'Example123#',
+        'confirm_password': 'Example123#'
+    }
+    client.cookies['access_token'] = tokens['access_token']
+    response = client.put(url, data, format='json')
+    mock_verification_email_task.assert_not_called()
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['code'] == 200
+    assert response.data['status'] == "success"
+    assert response.data['message'] == "User updated successfully."
+    assert response.data['data']
+    assert response.data['data']['id'] == str(user.id)
+
 
 def test_put_user_password_mismatch(mock_verification_email_task, client, user, signed_in_user):
     """
@@ -243,7 +216,7 @@ def test_put_user_password_length(mock_verification_email_task, client, user, si
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['password']
     assert response.data['errors']['password'][0] == "Password must be at least 8 characters long."
 
@@ -265,7 +238,7 @@ def test_put_user_password_without_digit(mock_verification_email_task, client, u
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['password']
     assert response.data['errors']['password'][0] == "Password must contain at least one digit."
 
@@ -286,7 +259,7 @@ def test_put_user_password_without_special_char(mock_verification_email_task, cl
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['password']
     assert response.data['errors']['password'][0] == "Password must contain at least one special character."
 
@@ -295,8 +268,6 @@ def test_put_user_password_without_letters(mock_verification_email_task, client,
     """
     Test update user with a password without letters.
     """
-    user.is_verified = True
-    user.save()
     url = reverse('user', kwargs={'id': user.id})
     data = {
         'password': '12345678#',
@@ -309,7 +280,7 @@ def test_put_user_password_without_letters(mock_verification_email_task, client,
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['code'] == 400
     assert response.data['status'] == "error"
-    assert response.data['message'] == "Validation failed."
+    assert response.data['message'] == "User update failed."
     assert response.data['errors']['password']
     assert response.data['errors']['password'][0] == "Password must contain at least one letter."
 
@@ -354,3 +325,59 @@ def test_put_user_without_access_token(client, user):
     assert response.data['message'] == "Authentication credentials were not provided."
 
 
+# test delete user
+def test_delete_user(client, user, signed_in_user):
+    """
+    Test delete user.
+    """
+    url  = reverse('user', kwargs={'id': user.id})
+    client.cookies['access_token'] = signed_in_user['access_token']
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['status'] == 'success'
+    assert response.data['code'] == 200
+    assert response.data['message'] == "User deleted successfully." 
+
+
+def test_delete_user_by_another_user(client, signed_in_user):
+    """
+    Test delete user by another user.
+    """
+    user = User.objects.create_user(
+        email="test-user@email.com",
+        password="Password123#")
+    url = reverse('user', kwargs={'id': user.id})
+    client.cookies['access_token'] = signed_in_user['access_token']
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.data['status'] == "error"
+    assert response.data['code'] == 403
+    assert response.data['message'] == "You do not have permission to perform this action."
+
+
+
+def test_delete_user_with_invalid_id(client, signed_in_user):
+    """
+    Test delete user with invalid id.
+    """
+    url = reverse('user', kwargs={'id': "45678909876543"})
+    client.cookies['access_token'] = signed_in_user['access_token']
+    response = client.get(url)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data['status'] == "error"
+    assert response.data['code'] == 400
+    assert response.data['message'] == "Invalid user id."
+
+
+def test_delete_admin_user(client, admin_user, signed_in_user):
+    """
+    Test delete admin user by regular user.
+    Admin users cannot be accessed from the url for reg user.
+    """
+    url = reverse('user', kwargs={'id': admin_user.id})
+    client.cookies['access_token'] = signed_in_user['access_token']
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data['status'] == "error"
+    assert response.data['code'] == 400
+    assert response.data['message'] == "Invalid user id."
