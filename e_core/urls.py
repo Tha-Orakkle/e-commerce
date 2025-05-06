837e444b85b237e39f384e6c9f0d08fa2a96a0ac
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-05-06 17:05:31 +0100

    update: add the products.urls to the main url file e_core.urls

--- e_core/urls.py
+++ e_core/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/', include('user.urls')),
+    path('api/v1/', include('product.urls')),
 ]
