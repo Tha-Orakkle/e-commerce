dd601b8e1d22286d4a389fab06a14e5f42568060
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-05-13 00:00:12 +0100

    update: add put and delete endpoints for products

--- product/api/v1/routes/product.py
+++ product/api/v1/routes/product.py
@@ -2,17 +2,21 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
-from common.utils.api_responses import (
-    SuccessAPIResponse,
-    ErrorAPIResponse
-)
+import uuid
+
+from common.utils.api_responses import SuccessAPIResponse
+from common.utils.check_valid_uuid import validate_id
 from common.utils.pagination import Pagination as PNP
+from common.exceptions import ErrorException
 from product.models import Product
 from product.serializers.product import ProductSerializer 
 
 
 class ProductView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         """
         Gets all products.
@@ -31,18 +35,86 @@ class ProductView(APIView):
 
 
     def post(self, request):
-        serializer = ProductSerializer(data=request.data)
+        """
+        Create a new product.
+        """
+        if not request.user.is_staff:
+            raise PermissionDenied()
+        serializer = ProductSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(
                 SuccessAPIResponse(
                     message="Product created successfully.",
-                    code=201
+                    code=201,
+                    data=serializer.data
                 ).to_dict(), status=status.HTTP_201_CREATED
             )
-        return Response(
-            ErrorAPIResponse(
-                message="Product creation failed.",
-                errors=serializer.errors
-            ).to_dict(), status=status.HTTP_400_BAD_REQUEST
+        raise ErrorException(
+            detail="Product creation failed.",
+            errors=serializer.errors
         )
+    
+
+class ProductDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
+        """
+        Get a specific product.
+        """
+        validate_id(id, "product")
+        product = Product.objects.filter(id=id).first()
+        if not product:
+            raise ErrorException("Invalid product id.")
+        serializer = ProductSerializer(product, context={'request': request})
+        return Response(
+            SuccessAPIResponse(
+                message="Product retrieved successfully.",
+                data=serializer.data
+            ).to_dict(), status=status.HTTP_200_OK
+        )
+        
+
+    
+    def put(self, request, id):
+        """
+        Update a specific product.
+        """
+        if not request.user.is_staff:
+            raise PermissionDenied()
+        validate_id(id, "product")
+        product = Product.objects.filter(id=id).first()
+        if not product:
+            raise ErrorException("Invalid product id.")
+        serializer = ProductSerializer(
+            instance=product,
+            data=request.data,
+            partial=True,
+            context={'request': request}    
+        )
+        if not serializer.is_valid():
+            raise ErrorException(
+                detail="Product update failed.",
+                errors=serializer.errors)
+        serializer.save()
+        return Response(
+            SuccessAPIResponse(
+                message="Product updated successfully.",
+                data=serializer.data
+            ).to_dict(), status=status.HTTP_200_OK
+        )
+        
+
+    def delete(self, request, id):
+        """
+        Delete a specific product.
+        """
+        if not request.user.is_staff:
+            raise PermissionDenied()
+        validate_id(id, "product")
+        product = Product.objects.filter(id=id).first()
+        if not product:
+          raise ErrorException("Invalid product id.")
+        product.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
