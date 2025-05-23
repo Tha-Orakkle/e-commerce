57472d0dd7089ed59fd84d87ce0f36ff3fbaf0f6
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-05-23 16:18:53 +0100

    update: change the ordering to descending for the user model

--- user/models.py
+++ user/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['staff_id'] # just for the creation of super user
 
     class Meta:
-        ordering = ['date_joined']
+        ordering = ['-date_joined']
     
     def __str__(self):
         """
