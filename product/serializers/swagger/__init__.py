14e0bbc7e80d21682db1c18c2433c18abb5d18f1
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-05-27 02:22:14 +0100

    doc: add swagger doc for category routes

--- product/serializers/swagger/__init__.py
+++ product/serializers/swagger/__init__.py
@@ -1,3 +1,10 @@
+from .category import (
+    create_category_schema,
+    delete_category_schema,
+    get_category_schema,
+    get_categories_schema,
+    update_category_schema
+)
 from .product import (
     create_product_schema,
     delete_product_schema,
@@ -13,13 +20,23 @@ from .product_image import (
 )
 
 __all__ = [
+    # product schemas
     'create_product_schema',
-    'create_product_image_schema',
     'delete_product_schema',
-    'delete_product_image_schema',
     'get_product_schema',
     'get_products_schema',
+    'update_product_schema',
+
+    # product image schemas
+    'create_product_image_schema',
+    'delete_product_image_schema',
     'get_product_image_schema',
     'get_product_images_schema',
-    'update_product_schema'
+
+    # category schemas
+    'create_category_schema',
+    'delete_category_schema',
+    'get_category_schema',
+    'get_categories_schema',
+    'update_category_schema'
 ]
