b794ff4fd3e76a420247762a5a2993040ef19347
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-05-31 02:13:06 +0100

    update: update product serializer to also add/remove product categories

--- product/serializers/product.py
+++ product/serializers/product.py
@@ -1,7 +1,9 @@
+from django.utils.text import slugify
 from rest_framework import serializers
 
-from product.models import Product
+from product.models import Product, Category
 from .product_image import ProductImageSerializer
+from .category import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,6 +11,7 @@ class ProductSerializer(serializers.ModelSerializer):
     Serializer for Product model.
     """
     images = ProductImageSerializer(read_only=True, many=True, required=False)
+    categories = CategorySerializer(read_only=True, many=True, required=False)
 
     class Meta:
         model = Product
@@ -19,7 +22,12 @@ class ProductSerializer(serializers.ModelSerializer):
         Create a Product instance.
         """
         image_data = self.initial_data.getlist('images', [])
+        category_list = self.initial_data.getlist('categories', [])
+
         product = Product.objects.create(**validated_data)
+
+        if category_list:
+            product.add_categories(category_list)
         if image_data:
             product.add_images(image_data)
         return product
@@ -29,9 +37,13 @@ class ProductSerializer(serializers.ModelSerializer):
         Update a Product instance.
         """
         image_data = self.initial_data.getlist('images', [])
+        category_list = self.initial_data.getlist('categories', [])
+
         for k, v in validated_data.items():
             setattr(instance, k, v)
         if image_data:
             instance.update_images(image_data)
+        if category_list:
+            instance.update_categories(category_list)
         instance.save()
         return instance
