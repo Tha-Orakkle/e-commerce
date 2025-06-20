316abd79075a8064e746864fbde5b243eb2f4759
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-06-20 23:46:13 +0100

    update docs to accommodate the request data

--- order/serializers/swagger/checkout.py
+++ order/serializers/swagger/checkout.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from common.swagger import (
     get_success_response,
     get_error_response_with_examples
@@ -5,6 +7,13 @@ from common.swagger import (
 from order.serializers.order import OrderSerializer
 
 # SWAGGER SCHEMAS FOR CHECKOUT
+class CheckoutRequestData(serializers.Serializer):
+    """
+    Serializer for the request data to checkou.
+    """
+    shipping_address = serializers.UUIDField(required=True)
+
+
 checkout_error_examples = {
     'Empty cart': 'Cart is empty.',
     'Invalid items': 'Cart contains invalid items.',
@@ -16,7 +25,7 @@ checkout_schema = {
     'description': 'Process the checkout of the current user\'s cart, creating an order and handling payment.',
     'operation_id': 'checkout',
     'tags': ['Checkout'],
-    'request': None, # wil be updated later to take the shipping/billing address
+    'request': CheckoutRequestData,
     'responses': {
         200: get_success_response('Checkout successful. Order has been created.', 200, OrderSerializer()),
         400: get_error_response_with_examples(examples=checkout_error_examples),
