7d159d6d103ff63c4143989d9fd4d1841b1dd287
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-07-01 21:43:22 +0100

    feat: create the payment model

---
+++ payment/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
+
+import uuid
+
+from order.models import Order
+
+
+class Payment(models.Model):
+    """
+    Payment model.
+    """ 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False)
+    reference = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField()
+    amount = models.PositiveBigIntegerField()
+    verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+
+    def __str__(self):
+        return f"<Payment: {self.reference}> {self.verified}"
+    
+    def to_dict(self):
+        return {
+            'reference': str(self.reference),
+            'email': self.email,
+            'amount': float(self.amount),
+            'callback_url': f"{settings.BASE_URL}{reverse('verify-payment')}"
+        }
