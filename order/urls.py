392c377f76655054ebb9686e8d82ef4eaa6ded3a
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-07-05 20:50:59 +0100

    update: add the order endpoints's urls

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from order.api.v1.routes.checkout import CheckoutView
+from order.api.v1.routes.order import (
+    OrdersView,
+    UserOrdersView,
+    UserOrderView
+)
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('orders/', OrdersView.as_view(), name='orders'),
+    path('orders/user/', UserOrdersView.as_view(), name='user-orders'),
+    path('orders/user/<str:order_id>/', UserOrderView.as_view(), name='user-order'),
 ]
