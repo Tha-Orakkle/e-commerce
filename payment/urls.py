132a774f9406170f49ef0912fc679fdae1906f3c
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-07-06 17:20:49 +0100

    update: update the payment urls file to accommodate the temporary callback endpoint

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
+from .api.v1.routes.webhook_paystack import PaystackWebhookView
 from .api.v1.services.initialize_paystack import InitializePaymentView
-from .api.v1.services.verify_paystack import VerifyPaymentView
+from .api.v1.services.verify_payment import VerifyPaymentView, TempCallback
 
 
 urlpatterns = [
+    path('paystack/webhook/', PaystackWebhookView.as_view(), name='paystack-webhook'),
+    
     path('payment/initialize/', InitializePaymentView.as_view(), name='initialize-payment'),
-    path('payment/verify/', VerifyPaymentView.as_view(), name='verify-payment')
+    path('payment/verify/', VerifyPaymentView.as_view(), name='verify-payment'),
+
+    # To be removed later
+    # Temporary callback for testing payment verification
+    path('payment/temporary-callback/', TempCallback.as_view(), name='temporary-callback'),
 ]
