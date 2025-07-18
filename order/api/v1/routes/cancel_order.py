9792ac0663b47734a02d896e69e3de595bd6cf19
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-07-18 23:20:27 +0100

    feat: add endpoint for users to cancel orders

---
+++ order/api/v1/routes/cancel_order.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+from django.utils.timezone import now
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from common.exceptions import ErrorException
+from common.utils.api_responses import SuccessAPIResponse
+from common.utils.check_valid_uuid import validate_id
+from order.tasks import restock_inventory_with_cancelled_order
+
+
+class CancelOrderView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, order_id):
+        """
+        Cancel an order by a user as long as it has not been
+        processed and not more than 4 hours.
+        """
+        validate_id(order_id, 'order')
+        order = request.user.orders.filter(id=order_id).first()
+        if not order:
+            raise ErrorException("Order not found.", code=status.HTTP_404_NOT_FOUND)
+        if order.status != 'PENDING':
+            raise ErrorException("Processed orders cannot be cancelled.", code=status.HTTP_400_BAD_REQUEST)
+        if order.created_at < now() - timedelta(hours=4):
+            raise ErrorException(
+                "Order cannot be cancelled after 4 hours of creation.",
+                code=status.HTTP_400_BAD_REQUEST
+            )
+        order.status = 'CANCELLED'
+        order.cancelled_at = now()
+        order.save()
+    
+        if order.payment_method == 'DIGITAL' and hasattr(order, 'payment') and order.payment.verified:
+            order.payment.refund_requested = True
+            order.payment.save()
+
+        restock_inventory_with_cancelled_order.delay(order.id)
+        return Response(SuccessAPIResponse(
+            message="Order cancelled. A refund will be processed shortly if payment was already made."
+        ).to_dict(), status=status.HTTP_200_OK)
