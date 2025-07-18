5d5def2a790628266fbb6555ae40bc4c00f51788
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-07-18 23:41:43 +0100

    update: add urls to cancel orders and for admin update

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from order.api.v1.routes.checkout import CheckoutView
+from order.api.v1.routes.cancel_order import CancelOrderView
+from order.api.v1.routes.admin_update_order_status import AdminUpdateOrderStatus
 from order.api.v1.routes.order import (
     OrdersView,
     UserOrdersView,
@@ -12,4 +14,7 @@ urlpatterns = [
     path('orders/', OrdersView.as_view(), name='orders'),
     path('orders/user/', UserOrdersView.as_view(), name='user-orders'),
     path('orders/user/<str:order_id>/', UserOrderView.as_view(), name='user-order'),
+    path('orders/user/<str:order_id>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
+
+    path('orders/<str:order_id>/update-status/', AdminUpdateOrderStatus.as_view(), name='update-order-status')
 ]
