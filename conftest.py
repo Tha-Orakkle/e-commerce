714b58f18ed53e6d385589d8d4289c4772aeecb4
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2025-12-06 03:52:43 +0000

    feat: create factory for users creation

--- conftest.py
+++ conftest.py
@@ -63,6 +63,62 @@ def db_access(db):
     pass
 
 
+# FACTORIES
+@pytest.fixture
+def customer_factory(db):
+    """
+    Factory to create customers.
+    """
+    def create_customer():
+        count = User.objects.filter(is_customer=True, is_superuser=False).count() + 1
+        customer = User.objects.create_user(
+            email=f"customer{count}@email.com",
+            password="Password123#",
+        )        
+        create_dummy_profile(customer)
+        Cart.objects.create(user=customer)
+        return customer
+    return create_customer
+
+@pytest.fixture
+def shopowner_factory(db):
+    """
+    Factory to create shop owners.
+    """
+    def create_shopowner():
+        count = User.objects.filter(is_shopowner=True, is_superuser=False).count() + 1
+        owner = User.objects.create_shopowner(
+            email=f"shopowner{count}@email.com",
+            staff_handle=f"shopowner{count}",
+            password="Password123#"
+        )
+        create_dummy_profile(owner)
+        Shop.objects.create(
+            name=f"Shop {count}",
+            description=f"Shop {count} created by shopowner factory.",
+            owner=owner
+        )
+        return owner
+    return create_shopowner
+
+@pytest.fixture
+def shop_staff_factory(db):
+    """
+    Factory to create shop staff.
+    """
+    def create_shop_staff(shopowner):
+        count = User.objects.filter(is_staff=True, is_superuser=False).count() + 1
+        staff = User.objects.create_staff(
+            shop=shopowner.owned_shop,
+            staff_handle=f"staff{count}",
+            password="Password123#"
+        )
+        create_dummy_profile(staff)
+        return staff
+    return create_shop_staff
+
+
+
 # USERS (SUPER USER, SHOP OWNER, SHOP STAFF AND CUSTOMER)
 @pytest.fixture
 def super_user(db):
@@ -76,63 +132,55 @@ def super_user(db):
     )
 
 @pytest.fixture
-def shopowner(db):
+def shopowner(shopowner_factory):
     """
     Creates a shop owner.
     """
-    owner = User.objects.create_shopowner(
-        email='shopowner@email.com',
-        staff_handle='shopowner',
-        password='Password123#'
-    )
-    create_dummy_profile(owner)
-    Shop.objects.create(
-        name="Playground",
-        description="Showcase you talents without fear of being judged.",
-        owner=owner
-    )
-    return owner
+    return shopowner_factory()
+    # owner = User.objects.create_shopowner(
+    #     email='shopowner@email.com',
+    #     staff_handle='shopowner',
+    #     password='Password123#'
+    # )
+    # create_dummy_profile(owner)
+    # Shop.objects.create(
+    #     name="Playground",
+    #     description="Showcase you talents without fear of being judged.",
+    #     owner=owner
+    # )
+    # return owner
 
 @pytest.fixture
-def shop_staff(db, shopowner):
+def shop_staff(db, shopowner, shop_staff_factory):
     """
     Create a staff for a shop.
     """
-    staff = User.objects.create_staff(
-        shop=shopowner.owned_shop,
-        staff_handle='staff',
-        password='Password123#'
-    )
-    create_dummy_profile(staff)
-    return staff
+    return shop_staff_factory(shopowner=shopowner)
+    # staff = User.objects.create_staff(
+    #     shop=shopowner.owned_shop,
+    #     staff_handle='staff',
+    #     password='Password123#'
+    # )
+    # create_dummy_profile(staff)
+    # return staff
 
 @pytest.fixture
-def customer(db):
+def customer(customer_factory):
     """
     Creates a customer.
     """
-    customer = User.objects.create_user(
-        email="customer@email.com",
-        password="Password123#", 
-    )
-    create_dummy_profile(customer)
-    Cart.objects.create(user=customer)
+    return customer_factory()
+    # customer = User.objects.create_user(
+    #     email="customer@email.com",
+    #     password="Password123#", 
+    # )
+    # create_dummy_profile(customer)
+    # Cart.objects.create(user=customer)
 
-    return customer
-
-@pytest.fixture
-def inactive_user(db):
-    """
-    Creates the inactive user
-    """
-    return User.objects.create_user(
-        email="inactiveuser@email.com",
-        password="Password123#",
-        is_active=False,
-    )
+    # return customer
 
 @pytest.fixture
-def all_users(super_user, shopowner, shop_staff, customer, inactive_user):
+def all_users(super_user, shopowner, shop_staff, customer):
     """
     Returns all user types as a dict.
     """
@@ -141,7 +189,6 @@ def all_users(super_user, shopowner, shop_staff, customer, inactive_user):
         'shopowner': shopowner,
         'shop_staff': shop_staff,
         'customer': customer,
-        'inactive_user': inactive_user
     }
 
 # to be removed start
@@ -298,3 +345,6 @@ def signed_in_admin(client, admin_user):
     }
 
 # to be removed ends
+
+# ==========================================================
+# Refactor fixtures
