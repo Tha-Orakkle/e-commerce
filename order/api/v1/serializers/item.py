c317dffa4a205399ad25df1204409ae33c231d42
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2025-12-18 11:53:15 +0000

    update: Remove ProductBriefSerializer and replace fields in OrderItemSerializer with the denormalized fields

--- order/api/v1/serializers/item.py
+++ order/api/v1/serializers/item.py
@@ -1,18 +1,10 @@
 from rest_framework import serializers
 
 from order.models import OrderItem
-from product.models import Product
-
-
-class ProductBriefSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'name']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = ProductBriefSerializer(read_only=True)
     
     class Meta:
         model = OrderItem
-        fields = ['id', 'product', 'quantity', 'price']
+        fields = ['id', 'product_name', 'product_description' 'quantity', 'price']
