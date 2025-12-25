6dc3efdcf1ba2ddff7f75bc4ff5a825c9a43097a
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2025-12-25 12:06:07 +0000

    feat: create serializer for uploading product images

--- product/api/v1/serializers/__init__.py
+++ product/api/v1/serializers/__init__.py
@@ -1,12 +1,13 @@
 from .category import CategorySerializer
 from .inventory import InventorySerializer
 from .product import ProductSerializer
-from .product_image import ProductImageSerializer
+from .product_image import ProductImageSerializer, UploadProductImageSeriallizer
 
 
 __all__ = [
     'CategorySerializer',
     'InventorySerializer',
     'ProductSerializer',
-    'ProductImageSerializer'
+    'ProductImageSerializer',
+    'UploadProductImageSeriallizer'
 ]
