809fe07274e4a5e47052d388220c40e2412b5959
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2025-12-27 02:46:58 +0000

    feat: create factories for products, product images and categories

--- product/tests/conftest.py
+++ product/tests/conftest.py
@@ -9,46 +9,6 @@ import shutil
 
 from product.models import Category, Product, ProductImage
 
-@pytest.fixture(scope="module")
-def shared_media_root(tmp_path_factory, request):
-    tmp_path = tmp_path_factory.mktemp('media')
-
-    # clean up
-    def cleanup():
-        if tmp_path.exists():
-            shutil.rmtree(tmp_path)
-
-    request.addfinalizer(cleanup)
-    yield tmp_path
-
-
-@pytest.fixture
-def temp_media_root(shared_media_root, settings):
-    settings.MEDIA_ROOT = shared_media_root
-
-
-@pytest.fixture
-def category(db):
-    """
-    Create a category instance for testing.
-    """
-    return Category.objects.create(
-        name="Test Category",
-        slug="test-category"
-    )
-
-
-@pytest.fixture
-def product(db, temp_media_root):
-    """
-    Create a product instance for testing.
-    """
-    return Product.objects.create(
-        name="Test Product",
-        description="This is a test product.",
-        price=9.99,
-    )
-
 
 def create_fake_images(n):
     """
@@ -67,6 +27,7 @@ def create_fake_images(n):
         ))
     return images
 
+
 def create_fake_files(n):
     """
     Create fake text files for testing.
@@ -83,22 +44,82 @@ def create_fake_files(n):
     return files
 
 
+@pytest.fixture(scope="module")
+def shared_media_root(tmp_path_factory, request):
+    tmp_path = tmp_path_factory.mktemp('media')
+
+    # clean up
+    def cleanup():
+        if tmp_path.exists():
+            shutil.rmtree(tmp_path)
+
+    request.addfinalizer(cleanup)
+    yield tmp_path
+
 
 @pytest.fixture
-def product_image(db, product):
+def temp_media_root(shared_media_root, settings):
+    settings.MEDIA_ROOT = shared_media_root
+
+# FACTORIES
+
+@pytest.fixture
+def product_factory(db, temp_media_root):
+    """
+    Factory to create products.
+    """
+    def create_product(shop):
+        count = Product.objects.filter(shop_id=shop.id).count()
+        return Product.objects.create(
+            name=f"Product {count}",
+            description=f"This is product {count}.",
+            price=99.9,
+            shop=shop
+        )
+    return create_product
+
+@pytest.fixture
+def product_image_factory(db, temp_media_root):
+    """
+    Factory to create product images.
+    """
+    
+    def create_product_image(product):
+        return ProductImage.objects.create(
+            product=product,
+            image=create_fake_images(1)[0]
+        )
+    return create_product_image
+
+@pytest.fixture
+def category_factory(db, temp_media_root):
+    """
+    Factory to create categories.
+    """
+    def create_category():
+        count = Category.objects.count()
+        return Category.objects.create(name=f"Test Category {count}")
+    
+    return create_category
+
+
+@pytest.fixture
+def product(db, shopowner, product_factory):
+    """
+    Create a product instance for testing.
+    """
+    return product_factory(shop=shopowner.owned_shop)
+
+@pytest.fixture
+def product_image(db, product, product_image_factory):
     """
     Create a product image instance for testing.
     """
-    return ProductImage.objects.create(
-        image=create_fake_images(1)[0],
-        product=product
-    )
-
+    return product_image_factory(product=product)
 
 @pytest.fixture
-def category(db):
+def category(db, category_factory):
     """
     Create a category instance for testing.
     """
-    return Category.objects.create(name="Test Category")
-
+    return category_factory()
