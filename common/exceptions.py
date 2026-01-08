983f0012bc98e07d07eae4857879904c77327353
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-01-08 02:18:54 +0000

    feat: implement exception for deleting inventory from its instance

--- common/exceptions.py
+++ common/exceptions.py
@@ -5,10 +5,17 @@ class ErrorException(APIException):
     """
     Handles API Exceptions.
     """
-    def __init__(self, detail=None, code='error', errors=None, status_code=400):
+    def __init__(self, detail=None, code='bad_request', errors=None, status_code=400):
         self.detail = detail
         self.code = code
         self.status_code = status_code
         if errors:
             self.errors = errors
         super().__init__(self.detail, self.status_code)
+        
+        
+class InventoryDeletionError(RuntimeError):
+    """
+    Raised when there is an attempt to delete an Inventory instance.
+    """
+    pass
