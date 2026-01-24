0eba2defdc642c57ab696a44a9e916880381bab9
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-01-24 17:57:09 +0000

    feat: create helper to populate cart

--- cart/tests/fixtures.py
+++ cart/tests/fixtures.py
@@ -2,7 +2,30 @@ from django.contrib.auth import get_user_model
 
 import pytest
 
+from cart.models import CartItem
+
 User = get_user_model()
+
+
+def fill_customer_cart(cart, shop, product_factory, count):
+    """
+    Fills a customers cart and fill it with products.
+    """
+    items = []
+    for _ in range(count):
+        prd = product_factory(shop=shop)
+        prd.inventory.add(20, handle='tester')
+        items.append(
+            CartItem(
+                quantity=15,
+                product=prd,
+                product_name=prd.name,
+                cart=cart
+            )
+        )
+    CartItem.objects.bulk_create(items)
+    cart.refresh_from_db()
+    return cart
 
 
 @pytest.fixture
