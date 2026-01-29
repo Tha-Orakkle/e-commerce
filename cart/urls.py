d2c21a67cf09d56ce51c6ec17978092aaace45ae
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-01-29 02:04:50 +0000

    update: change CartItemDetailView name from cart-item-update to cart-item-detail

--- cart/urls.py
+++ cart/urls.py
@@ -7,5 +7,5 @@ from .api.v1.routes import (
 
 urlpatterns = [
     path('customers/me/cart/', CartDetailView.as_view(), name='cart-detail'),
-    path('cart/items/<str:cart_item_id>/', CartItemDetailView.as_view(), name='cart-item-update')
+    path('cart/items/<str:cart_item_id>/', CartItemDetailView.as_view(), name='cart-item-detail')
 ]
