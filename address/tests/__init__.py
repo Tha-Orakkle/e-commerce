440bc44f9af72a43e0a876dd954f5c8c89eb1a05
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-02-07 15:13:30 +0000

    feat: create the address test dir and implement a fake location data in init file

---
+++ address/tests/__init__.py
@@ -0,0 +1,16 @@
+FAKE_LOCATION_DATA = [
+    {
+        'name': 'Nigeria',
+        'iso2': 'NG',
+        'states': [
+            {
+                'name': 'Lagos',
+                'cities': [
+                    {'name': 'Ikeja'},
+                    {'name': 'Lekki'},
+                    {'name': 'Yaba'}
+                ]
+            }
+        ]
+    }
+]
