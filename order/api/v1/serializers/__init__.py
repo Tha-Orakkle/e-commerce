c31aa8c98f91ed86a42770fe0227cd022c294ec0
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-06-21 11:58:16 +0000

    feat: create checkoutserializer to be used in the refactored checkoutview

--- order/api/v1/serializers/__init__.py
+++ order/api/v1/serializers/__init__.py
@@ -1,7 +1,9 @@
+from .checkout import CheckoutSerializer
 from .order_group import OrderGroupSerializer, OrderGroupListSerializer
 from .order import OrderSerializerForShop
 
 __all__ = [
+    'CheckoutSerializer',
     'OrderGroupSerializer',
     'OrderGroupListSerializer',
     'OrderSerializerForShop'
---
+++ order/api/v1/serializers/checkout.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+
+from address.models import ShippingAddress
+
+
+class CheckoutSerializer(serializers.Serializer):
+    """
+    Serializer to validate the shipping address, fulfillment
+    and payment method passed in request body.
+    """
+    
+    shipping_address = serializers.UUIDField()
+    payment_method = serializers.CharField()
+    fulfillment_method = serializers.CharField()
+    
+    def validate_shipping_address(self, value):
+        user = self.context["request"].user
+        
+        address = ShippingAddress.objects.filter(
+            user=user,
+            id=value
+        ).first()
+        if not address:
+            raise serializers.ValidationError(
+                "No shipping address matching the given ID found."
+            )
+        return address
+    
+    def validate_payment_method(self, value):
+        value = value.strip().upper()
+        allowed = ["CASH", "DIGITAL"]
+        
+        if value not in allowed:
+            raise serializers.ValidationError(
+                "The payment method must be either 'CASH' or 'DIGITAL'."
+            )
+            
+        return value
+    
+    def validate_fulfillment_method(self, value):
+        value = value.strip().upper()
+        
+        allowed = ["PICKUP", "DELIVERY"]
+        
+        if value not in allowed:
+            raise serializers.ValidationError(
+                "The payment method must be either 'CASH' or 'DIGITAL'."
+            )
+        return value
