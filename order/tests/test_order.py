37d64b5bfbafcbc0f858129c7f15f031c9558339
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-07-02 10:02:25 +0000

    test: implement tests for getting a specific shop order

--- order/tests/test_order.py
+++ order/tests/test_order.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 
 import pytest
+import uuid
 
 from order.models import Order
 
@@ -483,3 +484,211 @@ def test_get_all_shop_orders_ordered_by_invalid_ordering_parameter(
         str(order3.id),
         str(order1.id)
     ]
+
+
+# ======================================================
+# TEST GET A SPECIFIC SHOP ORDER
+# ======================================================
+@pytest.mark.parametrize(
+    "user_type",
+    ["shopowner", "shop_staff"],
+    ids=["shopowner", "shop_staff"]
+)
+def test_get_shop_order(client,
+                        populated_order_group_factory,
+                        user_type,
+                        all_users):
+    """
+    Test getting a specific shop order.
+    """
+    user = all_users[user_type]
+    if user.shop:
+        shop = user.shop
+    else:
+        shop = user.owned_shop
+
+    group = populated_order_group_factory(shop=shop)
+
+    order = group.orders.first()
+
+    url = reverse("shop-order", args=[order.id])
+    client.force_authenticate(user=user)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data["status"] == "success"
+    assert res.data["message"] == "Order retrieved successfully."
+
+    data = res.data["data"]
+    expected_fields = [
+        "id", "shipping", "items",
+        "status", "total_amount",
+        "is_paid", "shop_name",
+        "is_delivered", "is_picked_up",
+        "created_at", "delivery_date",
+        "paid_at", "processing_at",
+        "shipped_at", "completed_at",
+        "cancelled_at"
+    ]
+    assert all(field in data for field in expected_fields)
+
+    shipping = data["shipping"]
+    expected_shipping_fields = [
+        "full_name", "telephone",
+        "street_address", "city",
+        "state", "country",
+        "postal_code"
+    ]
+    assert all(field in shipping for field in expected_shipping_fields)
+
+    items = data["items"][0]
+    expected_item_fields = [
+        "id", "product_name",
+        "product_description",
+        "quantity", "price"
+    ]
+    assert all(field in items for field in expected_item_fields)
+
+
+def test_get_shop_order_counts(client,
+                               shopowner,
+                               populated_order_group_factory):
+    """
+    Test the order has all the order items
+    """
+    shop = shopowner.owned_shop
+    group = populated_order_group_factory(shop=shop,
+                                          items_per_order=5)
+
+    order = group.orders.first()
+
+    url = reverse("shop-order", args=[order.id])
+    client.force_authenticate(user=shopowner)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data["status"] == "success"
+    assert res.data["message"] == "Order retrieved successfully."
+
+    data = res.data["data"]
+    assert len(data["items"]) == 5
+
+
+def test_get_shop_order_by_customer(client,
+                                    customer,
+                                    shopowner_factory,
+                                    populated_order_group_factory):
+    """
+    test customer cannot get shop order.
+    """
+    shop = shopowner_factory().owned_shop
+    group = populated_order_group_factory(shop=shop)
+    order = group.orders.first()
+
+    url = reverse("shop-order", args=[order.id])
+    client.force_authenticate(user=customer)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_403_FORBIDDEN
+
+    data = res.data
+    assert data["status"] == "error"
+    assert data["code"] == "forbidden"
+    expected_msg = "You do not have permission to perform this action."
+    assert data["message"] == expected_msg
+
+
+@pytest.mark.parametrize(
+    "user_type",
+    ["shopowner", "shop_staff"],
+    ids=["shopowner", "shop_staff"]
+)
+def test_get_shop_order_by_different_shop_staff(
+    client,
+    shopowner_factory,
+    populated_order_group_factory,
+    all_users,
+    user_type
+):
+    """
+    Test getting shop order by a different shopowner or
+    shop staff.
+    """
+    shop = shopowner_factory().owned_shop
+    user = all_users[user_type]
+    group = populated_order_group_factory(shop=shop)
+    order = group.orders.first()
+
+    url = reverse("shop-order", args=[order.id])
+    client.force_authenticate(user=user)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+    assert res.data["status"] == "error"
+    assert res.data["code"] == "not_found"
+    assert res.data["message"] == "No order matching the given ID found."
+
+
+def test_get_shop_order_with_non_existent_order_id(
+    client,
+    shopowner
+):
+    """
+    Test getting shop order with non-existent order id.
+    """
+    url = reverse("shop-order", args=[uuid.uuid4()])
+    client.force_authenticate(user=shopowner)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+    assert res.data["status"] == "error"
+    assert res.data["code"] == "not_found"
+    assert res.data["message"] == "No order matching the given ID found."
+
+
+def test_get_shop_order_with_invalid_uuid(
+    client,
+    shopowner
+):
+    """
+    Test getting shop order with invalid order id.
+    """
+    url = reverse("shop-order", args=["invalid-uuid"])
+    client.force_authenticate(user=shopowner)
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data["status"] == "error"
+    assert res.data["code"] == "invalid_uuid"
+    assert res.data["message"] == "Invalid order id."
+
+
+
+def test_get_shop_order_by_unauthenticated_user(
+    client,
+    populated_order_group_factory
+):
+    """
+    Test getting shop order by unauthenticated user fails.
+        - Test with no credentials provided and with invalid
+          access token.
+    """
+    group = populated_order_group_factory()
+    order = group.orders.first()
+    url = reverse("shop-order", args=[order.id])
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+    assert res.data['status'] == "error"
+    assert res.data['code'] == "unauthorized"
+    expected_msg = "Authentication credentials were not provided."
+    assert res.data['message'] == expected_msg
+
+    client.cookies['access_token'] = "Invalid_access_token2445"
+    res = client.get(url)
+
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+    assert res.data['status'] == "error"
+    assert res.data['code'] == "unauthorized"
+    assert res.data['message'] == "Token is invalid or expired"
+    
