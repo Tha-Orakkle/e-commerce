f8dcf82eb5be3ab58f40a4b628079c79854ffa68
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-08-01 00:26:59 +0000

    update: change the initialize payment view

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .api.v1.services import InitializePaystackView
 from .api.v1.routes import (
+    InitializePaymentView,
     VerifyPaymentView,
     PaystackWebhookView,
     TempCallback
@@ -11,7 +11,7 @@ from .api.v1.routes import (
 urlpatterns = [
     path('paystack/webhook/', PaystackWebhookView.as_view(), name='paystack-webhook'),
     
-    path('order-groups/<str:order_group_id>/payment/initialize/', InitializePaystackView.as_view(), name='initialize-payment'),
+    path('order-groups/<str:order_group_id>/payment/initialize/', InitializePaymentView.as_view(), name='initialize-payment'),
     path('payment/verify/<str:reference>/', VerifyPaymentView.as_view(), name='verify-payment'),
 
     # To be removed later
