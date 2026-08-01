7d722359a64df1e16fb258dc9c75ba5958447ec0
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2026-08-01 18:25:28 +0000

    update: create serializer to handle initialize payment input

---
+++ payment/api/v1/serializers/service.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from payment.services import SERVICE_MAP
+
+
+class InitializePaymentSerializer(serializers.Serializer):
+    """
+    Serializer to handle the payment service provider.
+    """
+    service = serializers.CharField()
+
+    def validate_service(self, value):
+        value = value.strip().lower()
+
+        if value not in SERVICE_MAP:
+            raise serializers.ValidationError(
+                "Input a supported service type."
+            )
+        return SERVICE_MAP[value]
